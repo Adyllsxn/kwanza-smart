5ad80e1a304fa76ea7e73db64c87ab75a3389929
Adyllsxn <domingos.nxscimento@gmail.com>
2026-04-23 14:48:58 +0100

    add README.md

--- src/api/LearnAPI/Source/Core/Application/Modules/ApplicationModule.cs
+++
@@ -1,8 +0,0 @@
-namespace LearnAPI.Source.Core.Application.Modules;
-public static class ApplicationModule
-{
-    public static void AddApplication(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddScoped<IUserService, UserService>();
-    }
-}
--- src/api/LearnAPI/Source/Core/Infrastructure/Data/Context/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-
-namespace LearnAPI.Source.Core.Infrastructure.Data.Context;
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-{
-    public DbSet<UserEntity> Users { get; set; }
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(typeof(InfrastructureModule).Assembly);
-    
-}
